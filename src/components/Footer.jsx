fa96a07a31a3967e8b178ce6d7d29e3a8a7de139
Oooooiii <hearong.200@gmail.com>
2021-12-04 17:54:13 +0700

    ADD: footer design

---
+++ src/components/Footer.jsx
@@ -0,0 +1,135 @@
+import React from 'react';
+import styled from 'styled-components';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import EmailIcon from '@mui/icons-material/Email';
+
+const Footer = () => {
+  return (
+    <Container>
+      <Left>
+        <Logo>ecommerce</Logo>
+        <Desc>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda, voluptatum.</Desc>
+        <SocialContainer>
+          <SocialIcon color="3b5999">
+            <FacebookIcon />
+          </SocialIcon>
+          <SocialIcon color="e4405f">
+            <InstagramIcon />
+          </SocialIcon>
+          <SocialIcon color="55acee">
+            <TwitterIcon />
+          </SocialIcon>
+          <SocialIcon color="e60023">
+            <PinterestIcon />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Woman Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <LocationOnIcon style={{ marginRight: '10px' }} />
+          432 Dixiw Path, South Tobinchester 84732
+        </ContactItem>
+        <ContactItem>
+          <LocalPhoneIcon style={{ marginRight: '10px' }} />
+          +1 383 242 23
+        </ContactItem>
+        <ContactItem>
+          <EmailIcon style={{ marginRight: '10px' }} />
+          contact@gmail.com
+        </ContactItem>
+        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+      </Right>
+    </Container>
+  );
+};
+
+export default Footer;
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+const Logo = styled.h1``;
+
+const Desc = styled.p`
+  margin: 20px 0;
+`;
+
+const SocialContainer = styled.div`
+  display: flex;
+`;
+
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 10px;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+const Payment = styled.img`
+  width: 50%;
+`;
